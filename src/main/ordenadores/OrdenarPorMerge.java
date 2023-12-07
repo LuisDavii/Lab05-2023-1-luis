7de7d6506e1a9643f62b625d07c42fe7fb0abf83
Luis Davi <136763421+LuisDavii@users.noreply.github.com>
2023-12-07 20:30:08 +0000

    merge

--- src/main/ordenadores/OrdenarPorMerge.java
+++ src/main/ordenadores/OrdenarPorMerge.java
@@ -6,9 +6,26 @@ public class OrdenarPorMerge implements Ordenador{
         return array;
     }
 
-    public int[] merge(int[] array, int[] array2) {
-        
-        return array;
+    public int[] merge(int[] array1, int[] array2) {
+        int[] mArray = new int[array1.length + array2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < array1.length && j < array2.length) {
+            if (array1[i] < array2[j]) {
+                mArray[k++] = array1[i++];
+            } else {
+                mArray[k++] = array2[j++];
+            }
+        }
+
+        while (i < array1.length) {
+            mArray[k++] = array1[i++];
+        }
+
+        while (j < array2.length) {
+            mArray[k++] = array2[j++];
+        }
+
+        return mArray;
     }
     
 }
